66dd16d5db93f68fde8cd002aa036214cf293cbf
Ewan Stott <ewan.stott1@gmail.com>
2024-01-27 16:43:13 +0100

    hw - adding weather API using axios

--- ch16-react-demo-2/src/App.js
+++ ch16-react-demo-2/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
-import Child from "./components/Child";
+import Spinner from "./components/Spinner";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {};
+
+  async componentDidMount() {
+    const { data } = await axios.get(
+      "https://api.openweathermap.org/data/2.5/weather?lat=2.22&lon=2.22&appid={adef24c0d24a95065818997a98ddd457}"
+    );
+    // const { data } = await axios.get(https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={adef24c0d24a95065818997a98ddd457});
+    console.log(data);
+  }
+
   render() {
-    return <Child />;
+    return <Spinner />;
   }
 }
 
---
+++ ch16-react-demo-2/src/components/Spinner.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Spinner extends Component {
+  state = {};
+  render() {
+    return (
+      <div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Spinner;
